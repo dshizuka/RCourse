eeb699006df99e1080731c2ee6194656eb9160f9
Dai Shizuka <daishizuka@gmail.com>
2019-12-09 22:36:48 -0600

    Update tidytuesday_20191203.R

--- tidytuesday/tidytuesday_20191203.R
+++ tidytuesday/tidytuesday_20191203.R
@@ -1,1 +1,14 @@
+library(tidyverse)
+library(ggmap)
+
 tickets <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-12-03/tickets.csv")
+
+tickets
+
+dbpk=tickets %>% filter(violation_desc=="DOUBLE PARKED")
+
+plot(dbpk$lon, dbpk$lat, pch=19, col=gray(0.5, 0.3), las=1)
+
+
+
+qmplot(lon, lat, data = dbpk, maptype = "toner-lite", color = "tomato")
