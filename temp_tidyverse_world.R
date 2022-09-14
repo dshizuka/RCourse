c8aaefe2b71b8e732792490d75fdeeb1ef7f3f96
Daizaburo Shizuka <dshizuka2@unl.edu>
2022-09-14 11:10:51 -0500

    world bank data

--- temp_tidyverse_world.R
+++ temp_tidyverse_world.R
@@ -25,3 +25,24 @@ d=codelist %>% select(country.name.en, iso2c)
 as.data.frame(d)
 
 match(dat$iso2c, codelist$iso2c)
+
+library(tidyverse)
+
+world_bank_pop
+unique(world_bank_pop$country)
+
+?countrycode
+cnames=countrycode(unique(world_bank_pop$country), origin='iso3c', destination='country.name')
+length(which(is.na(cnames)==F))
+
+
+countries_list=codelist %>% select(country.name.en, iso2c, iso3c)
+countries_list
+
+df=inner_join(world_bank_pop, countries_list, by=c("country"="iso3c"))
+nrow(df)
+length(unique(df$country))
+nrow(world_bank_pop)
+
+
+wb_countries()
