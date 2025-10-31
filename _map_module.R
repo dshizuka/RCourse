9a5ddcefdbfe79726a61a7fa4dc4f9eeff8fc2b3
Dai <dshizuka2@unl.edu>
2025-10-31 15:07:00 -0500

    map making

--- _map_module.R
+++ _map_module.R
@@ -79,4 +79,44 @@ nemap
 ####
 #sf
 
+#download shapefile and use it.
+library(sf)
 
+#e.g., I downloaded US county data from US census bureau: https://www.census.gov/geographies/mapping-files/time-series/geo/carto-boundary-file.html
+
+us_shp=st_read("data/cb_2018_us_county_20m/cb_2018_us_county_20m.shp")
+
+ggplot() +
+  geom_sf(data=us_shp, fill="goldenrod") +
+  coord_sf(xlim=c(-180,-50)) +
+  theme_bw()
+
+co_data=read.csv("data/co-est2024-alldata.csv")
+head(co_data)
+
+us_shp %>% filter(STATEFP==1)
+
+#downloaded NE counties data from https://www.nebraskamap.gov/datasets/county-boundaries-2/explore
+ne=st_read("data/NE_County_Boundaries/County_Boundaries.shp")
+
+ne_data=co_data %>% filter(STNAME=="Nebraska")
+
+ne_full2024=ne %>% mutate(CountyFIPS=as.numeric(CountyFIPS)) %>% left_join(co_data %>% filter(STNAME=="Nebraska"), by=join_by("CountyFIPS"=="COUNTY"))
+
+ggplot() +
+  geom_sf(data=ne_full2024, aes(fill=POPESTIMATE2024)) +
+  theme_bw()
+
+ggplot() +
+  geom_sf(data=ne_full2024, aes(fill=RBIRTH2024)) +
+  theme_bw()
+
+unl=data.frame(long=693904.69351446, lat=4521004.547643)
+
+ggplot() +
+  geom_sf(data=ne_full2024, aes(fill=NPOPCHG2024)) +
+  theme_bw() +
+  geom_point(data=unl, aes(x=long, y=lat))
+
+##
+library(tigris)
